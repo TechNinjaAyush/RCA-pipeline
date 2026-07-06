425cddf4981b40bdbe24eced2f150d0be0cc9534
TechNinjaAyush <ayushkhairnar06@gmail.com>
2026-07-06 21:55:50 +0530

    Added dockerignore file

--- rca_agent/config/settings.py
+++ rca_agent/config/settings.py
@@ -6,17 +6,17 @@ load_dotenv()
 
 class Settings:
     # NATS Settings
-    NATS_URL = os.getenv("NATS_URL", "nats://localhost:4222")
-    NATS_SUBJECT = os.getenv("NATS_SUBJECT", "pod.alert")
+    NATS_URL = os.getenv("NATS_URL", "nats://nats:4222")
+    NATS_SUBJECT = os.getenv("NATS_SUBJECT", "traffic.alert")
 
     # NATS Human-in-the-Loop subjects (published by Slack Bolt action handlers)
     NATS_APPROVE_SUBJECT = os.getenv("NATS_APPROVE_SUBJECT", "remediation.approved")
     NATS_REJECT_SUBJECT  = os.getenv("NATS_REJECT_SUBJECT",  "remediation.rejected")
 
     # Neo4j Settings
-    NEO4J_URI      = os.getenv("NEO4J_URI",      "http://localhost:7687")
+    NEO4J_URI      = os.getenv("NEO4J_URI",      "bolt://neo4j:7687")
     NEO4J_USER     = os.getenv("NEO4J_USER",     "neo4j")
-    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
+    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "your_password")
 
     # Gmail Settings (kept for backward compat, not used in Slack flow)
     GMAIL_SENDER       = os.getenv("GMAIL_SENDER",       "")
